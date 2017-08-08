3370cf540b6223cacc2081ed473c62b15401c415
msadeghifar <msadeghifar18@gmail.com>
2017-08-08 13:39:54 +0430

    change views of login-register-verify pages and directions

--- Script/Routes/routes.js
+++ Script/Routes/routes.js
@@ -5,15 +5,15 @@
     .config(function($stateProvider, $urlRouterProvider){
       $stateProvider
         .state('home', {url:"/", templateUrl:"Views/home.html"})
-        .state('account', {url: "/account", templateUrl:"Views/account.html",
+        .state('account', {url: "/account", templateUrl:"Views/account/index.html",
           controller: function($state){
             $state.go("login");
           }
         })
-        .state('login', {parent: "account", url: "/login", templateUrl:"Views/login.html"})
-        .state('register', {parent: "account", url: "/register", templateUrl:"Views/register.html"})
-        .state('verify', {parent: "account", url: "/verify", templateUrl:"Views/verify.html"})
-        .state('forget', {parent: "account", url: "/forget", templateUrl:"Views/forget.html"})
+        .state('login', {parent: "account", url: "/login", templateUrl:"Views/account/login.html"})
+        .state('register', {parent: "account", url: "/register", templateUrl:"Views/account/register.html"})
+        .state('verify', {parent: "account", url: "/verify", templateUrl:"Views/account/verify.html"})
+        .state('forget', {parent: "account", url: "/forget", templateUrl:"Views/account/forget.html"})
         .state('detail', {parent: "account", url: "/detail", templateUrl:"Views/detail.html"})
         .state('orders', {url:"/orders", templateUrl:"Views/orders.html"})
         .state('checkout', {url:"/checkout", templateUrl:"Views/checkout.html"})
